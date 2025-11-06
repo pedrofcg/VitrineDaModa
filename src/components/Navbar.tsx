4b79c7519a46af452f594ba62fe0ac8cda6c335e
Pedro Felipe <134977983+pedrofcg@users.noreply.github.com>
2025-11-06 14:00:17 -0300

    Cart ICON remove

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,12 +44,7 @@ const Navbar = () => {
 
           {/* Cart & Mobile Menu Button */}
           <div className="flex items-center space-x-4">
-            <Button variant="ghost" size="icon" className="relative">
-              <ShoppingBag className="h-5 w-5" />
-              <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-[10px] font-bold text-primary-foreground flex items-center justify-center">
-                0
-              </span>
-            </Button>
+            
 
             {/* Mobile Menu Toggle */}
             <button
