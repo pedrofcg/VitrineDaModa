7366f64b433fcb91b0e2d3e8238167dc5d6817bf
Pedro Felipe <134977983+pedrofcg@users.noreply.github.com>
2026-01-20 00:46:37 -0300

    Add new images and update product titles

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,6 +35,50 @@ import ShortBeachTenis from "@/assets/ShortBeachTenis.jpg";
 import ShortBeachTenisBranco from "@/assets/ShortBeachTenisBranco.jpg";
 import ShortBeachTenisBranco01 from "@/assets/ShortBeachTenisBranco01.jpg";
 import ShortBeachTenisBranco03 from "@/assets/ShortBeachTenisBranco03.jpg";
+import azul3 from "@/assets/azul3.jpeg";
+import azul4 from "@/assets/azul4.jpeg"; 
+import AzulEscuro2 from "@/assets/AzulEscuro2.jpeg";
+import rosa1 from "@/assets/rosa1.jpeg";
+import rosa2 from "@/assets/rosa2.jpeg";
+import rosa3 from "@/assets/rosa3.jpeg";
+import verde3 from "@/assets/verde3.jpeg";
+import roxo from "@/assets/roxo.jpeg";
+import vermelho1 from "@/assets/vermelho1.jpeg";
+import vinho1 from "@/assets/vinho1.jpeg";
+import vinho2 from "@/assets/vinho2.jpeg";
+import vinho3 from "@/assets/vinho3.jpeg";
+import vinho4 from "@/assets/vinho4.jpeg";
+import vermelho2 from "@/assets/vermelho2.jpeg";
+import vermelho3 from "@/assets/vermelho3.jpeg";
+import vermelho4 from "@/assets/vermelho4.jpeg";
+import verde from "@/assets/verde.jpeg";
+import verde2 from "@/assets/verde2.jpeg";
+import verde4 from "@/assets/verde4.jpeg";
+import AzulEscuro1 from "@/assets/AzulEscuro1.jpeg";
+import AzulEscuro3 from "@/assets/AzulEscuro3.jpeg";
+import AzulEscuro4 from "@/assets/AzulEscuro4.jpeg";
+import AzulClaro1 from "@/assets/AzulClaro1.jpeg";
+import AzulClaro2 from "@/assets/AzulClaro2.jpeg";
+import AzulClaro3 from "@/assets/AzulClaro3.jpeg";
+import AzulClaro4 from "@/assets/AzulClaro4.jpeg";
+import AzulClaro5 from "@/assets/AzulClaro5.jpeg";
+import AzulClaro6 from "@/assets/AzulClaro6.jpeg";
+import marrom1 from "@/assets/marrom1.jpeg";
+import marrom2 from "@/assets/marrom2.jpeg";
+import roxo2 from "@/assets/roxo2.jpeg";
+import roxo3 from "@/assets/roxo3.jpeg";
+import lilas1 from "@/assets/lilas1.jpeg";
+import lilas2 from "@/assets/lilas2.jpeg";
+import lilas3 from "@/assets/lilas3.jpeg";  
+import lilas4 from "@/assets/lilas4.jpeg";
+import lilas5 from "@/assets/lilas5.jpeg";
+import lilas6 from "@/assets/lilas6.jpeg";
+import lilas7 from "@/assets/lilas7.jpeg";
+import roxo4 from "@/assets/roxo4.jpeg";
+import roxo5 from "@/assets/roxo5.jpeg";
+import roxo6 from "@/assets/roxo6.jpeg";
+
+
 
 const Index = () => {
   const featuredProducts = [
@@ -44,6 +88,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoPreto],
     },
     {
       image: ConjuntoAzul,
@@ -51,6 +96,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoAzul],
     },
     {
       image: ConjuntoRosa,
@@ -58,6 +104,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoRosa],
     },
     {
       image: ConjuntoRoxo,
@@ -65,6 +112,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoRoxo],
     },
     {
       image: ConjuntoMarrom,
@@ -72,6 +120,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoMarrom],
     },
     {
       image: ConjuntoVinho,
@@ -79,6 +128,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoVinho],
     },
     {
       image: ConjuntoLaranja,
@@ -86,6 +136,7 @@ const Index = () => {
       price: 69.90,
       originalPrice: 89.90,
       sizes: ["P", "M"],
+      images: [ConjuntoLaranja],
     },
     {
       image: BlusaNoTuleAzulClaro,
@@ -93,6 +144,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleAzulClaro],
     },
     {
       image: BlusaNoTuleBeje,
@@ -100,6 +152,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleBeje],
     },
     {
       image: BlusaNoTuleBranco,
@@ -107,6 +160,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleBranco],
     },
     {
       image: BlusaNoTuleMarrom,
@@ -114,6 +168,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleMarrom],
     },
     {
       image: BlusaNoTuleRosaClaro,
@@ -121,6 +176,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleRosaClaro],
     },
     {
       image: BlusaNoTuleRosa,
@@ -128,6 +184,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleRosa],
     },
     {
       image: BlusaNoTulePreto,
@@ -135,6 +192,7 @@ const Index = () => {
       price: 32.99,
       originalPrice: 49.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTulePreto],
     },
     {
       image: ConjuntoCaneladoBicolor,
@@ -142,6 +200,7 @@ const Index = () => {
       price: 129.99,
       originalPrice: 149.99,
       sizes: ["M", "G"],
+      images: [ConjuntoCaneladoBicolor, ConjuntoCaneladoBicolor01],
     },
     {
       image: ConjuntoCaneladoRosa01,
@@ -149,6 +208,7 @@ const Index = () => {
       price: 119.90,
       originalPrice: 139.90,
       sizes: ["M", "G"],
+      images: [ConjuntoCaneladoRosa, ConjuntoCaneladoRosa01],
     },
     {
       image: ConjuntoPoliamidaAzulClaro01,
@@ -156,6 +216,7 @@ const Index = () => {
       price: 109.90,
       originalPrice: 129.90,
       sizes: ["M", "G"],
+      images: [ConjuntoPoliamidaAzulClaro, ConjuntoPoliamidaAzulClaro01],
     },
     {
       image: ConjuntoPoliamidaBranco,
@@ -163,6 +224,7 @@ const Index = () => {
       price: 119.90,
       originalPrice: 139.90,
       sizes: ["M", "G"],
+      images: [ConjuntoPoliamidaBranco, ConjuntoPoliamidaBranco01],
     },
     {
       image: ShortBeachTenis,
@@ -170,15 +232,105 @@ const Index = () => {
       price: 99.90,
       originalPrice: 129.90,
       sizes: ["P", "M", "G"],
+      images: [ShortBeachTenis],
     },
     {
       image: ShortBeachTenisBranco,
-      title: "Short Beach Tênis Com Bolso",
+      title: "Short Beach Tênis",
       price: 99.90,
       originalPrice: 129.90,
       sizes: ["P", "M", "G"],
-      
+      images: [ShortBeachTenisBranco, ShortBeachTenisBranco01, ShortBeachTenisBranco03],
     },
+    {
+      image: azul3,
+      title: "Macacão sem costura",
+      price: 129.90,
+      originalPrice: 139.90,
+      sizes: ["P", "M", "G"],
+      images: [azul3, azul4],
+    },
+    {
+      image: AzulEscuro2,
+      title: "Conjunto Canelado Bicolor",
+      price: 159.99,
+      originalPrice: 169.90,
+      sizes: ["P", "M", "G"],
+      images: [AzulEscuro2, AzulEscuro1, AzulEscuro3, AzulEscuro4],
+    },
+    {
+      image: rosa1,
+      title: "Conjunto Canelado",
+      price: 139.99,
+      originalPrice: 149.99,
+      sizes: ["P", "M", "G"],
+      images: [rosa1, rosa2, rosa3],
+    },
+    {
+      image: verde3,
+      title: "Conjunto Canelado Jacquard",
+      price: 149.99,
+      originalPrice: 169.90,
+      sizes: ["P", "M", "G"],
+      images: [verde , verde2, verde3, verde4],
+    },
+    {
+      image: roxo,
+      title: "Conjunto Canelado Poliamida",
+      price: 119.99,
+      originalPrice: 129.90,
+      sizes: ["P", "M", "G"],
+      images: [roxo, roxo2, roxo3],
+    },
+    {
+      image: vermelho1,
+      title: "Conjunto Canelado",
+      price: 159.99,
+      originalPrice: 169.90,
+      sizes: ["P", "M", "G"],
+      images: [vermelho1 , vermelho2, vermelho3, vermelho4],
+    },
+    {
+      image: vinho4,
+      title: "Macaquinho Sem Costura",
+      price: 119.99,
+      originalPrice: 129.90,
+      sizes: ["P", "M", "G"],
+      images: [vinho1, vinho2, vinho3, vinho4],
+    },
+    {
+      image: AzulClaro1,
+      title: "Conjunto Canelado Mula Manca",
+      price: 159.99,
+      originalPrice: 169.90,
+      sizes: ["P", "M", "G"],
+      images: [AzulClaro1, AzulClaro2, AzulClaro3, AzulClaro4, AzulClaro5, AzulClaro6],
+    },
+    {
+      image: marrom1,
+      title: "Macacão Longo Canelado",
+      price: 169.99,
+      sizes: ["P", "M", "G"],
+      images: [marrom1, marrom2],
+    },
+    {
+      image: lilas1,
+      title: "Conjunto IZA Canelado",
+      price: 159.99,
+      originalPrice: 169.90,
+      sizes: ["P", "M", "G"],
+      images: [lilas1, lilas2, lilas3, lilas4, lilas5, lilas6, lilas7],
+    },
+    {
+      image: roxo4,
+      title: "Conjunto Canelado",
+      price: 139.99,
+      sizes: ["P", "M", "G"],
+      images: [roxo4, roxo5, roxo6],
+    },
+
+
+    
   
   ];
 
@@ -188,24 +340,28 @@ const Index = () => {
       title: "Blusa No Tule Rosa Claro",
       price: 32.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleRosaClaro],
     },
     {
       image: ConjuntoPreto,
       title: "Conjunto No Suplex",
       price: 69.90,
       sizes: ["P", "M"],
+      images: [ConjuntoPreto],
     },
     {
       image: BlusaNoTuleAzulClaro,
       title: "Blusa No Tule Azul Claro",
       price: 32.99,
       sizes: ["P", "M"],
+      images: [BlusaNoTuleAzulClaro],
     },
     {
       image: ConjuntoRosa,
       title: "Conjunto No Suplex",
       price: 69.90,
       sizes: ["P", "M"],
+      images: [ConjuntoRosa],
     },
   ];
 
@@ -231,7 +387,7 @@ const Index = () => {
               Vista sua melhor versão
             </h1>
             <p className="text-base md:text-xl text-white/90 mb-6 md:mb-8">
-              Moda fitness que combina estilo, conforto e performance para mulheres que não abrem mão de nada.
+              Cada peça é criada com tecidos de alta qualidade, cuidadosamente selecionados para oferecer conforto absoluto, excelente caimento e durabilidade, valorizando seu corpo e acompanhando você em todos os momentos — do treino intenso à rotina do dia a dia
             </p>
             <div className="flex flex-col sm:flex-row gap-3 md:gap-4">
               <Link to="/colecoes" className="w-full sm:w-auto">
